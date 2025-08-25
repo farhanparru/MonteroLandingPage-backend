bcdaffdf97113578a86521c1c33baadf8e37c0db
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-08-25 05:46:52 -0500

    server.js cors local URL change replace production URL

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const signupRoute = require('./routes/signup')
 const app = express()
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://montero-landing.onrender.com'],
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true, 
     allowedHeaders: ["Content-Type", "Authorization"],
